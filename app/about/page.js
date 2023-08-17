9b2a3a5f5a91675446a0e7aaef6ef936d3e02fdf
Filipe Valente Gomes <filipego@gmail.com>
2023-08-17 15:24:39 -0400

    added a page

---
+++ app/about/page.js
@@ -0,0 +1,12 @@
+import Container from "../components/Container"
+
+
+export default async function AboutPage() {
+
+
+    return (
+        <Container>
+            <h2>About Page</h2>
+        </Container>
+    )
+}
--- app/components/Header.js
+++ app/components/Header.js
@@ -1,7 +1,11 @@
+import Link from "next/link";
+import Nav from "./Nav";
+
 export default function Header() {
     return (
         <header>
-            Header
+            <Link href='/'>Logo</Link>
+            <Nav />
         </header>
     )
 }
